4c6d3ac5fa446302627df3f0d954f79ba7eba744
yvonne <yvosobrero2@hotmail.com>
2025-04-03 13:48:49 +0200

    test

--- catalog/views.py
+++ catalog/views.py
@@ -47,7 +47,7 @@ class BookDetailView(generic.DetailView):
     
 class AuthorListView(generic.ListView):
     model = Author
-    paginate_by = 3
+    paginate_by = 10
 
 class AuthorDetailView(generic.DetailView):
     model = Author    
