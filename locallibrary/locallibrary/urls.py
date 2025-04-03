1dfac135dd51f05e5c4b19c6799449908c265432
yvonne <yvosobrero2@hotmail.com>
2025-04-03 11:04:40 +0200

    comentario

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -39,3 +39,6 @@ urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += [
     path('catalog/', include('catalog.urls')),
 ]
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -25,6 +25,7 @@ from django.urls import include
 
 urlpatterns += [
 	path('catalog/', include('catalog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 from django.views.generic import RedirectView
@@ -36,4 +37,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
